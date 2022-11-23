fec637d231521d64d5247b6b06c2d43c65a7f624
vesnatrisic <trisic.vesna1@gmail.com>
2022-11-23 09:40:00 +0100

    zadatak

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path ("", views.index, name="index"),
-    path ("Python", views.python, name="python")
+    path ("Python", views.python, name="python"),
+    path ("CSS", views.css, name="css"),
+    path ("Django", views.django, name="django"),
+    path ("GIT", views.git, name="git"),
+    path ("HTML", views.html, name="html"),
+    path ("latestNews", views.latestNews, name="latestNews")
 ]
--- hello/views.py
+++ hello/views.py
@@ -7,3 +7,20 @@ def index(request):
 
 def python(request):
     return render (request, "hello/python.html")
+
+def css(request):
+    return render (request, "hello/css.html")
+
+def django(request):
+    return render (request, "hello/django.html")
+
+def git(request):
+    return render (request, "hello/git.html")
+
+def html(request):
+    return render (request, "hello/html.html")
+
+def latestNews(request):
+    return render (request, "hello/latestNews.html")
+
+
